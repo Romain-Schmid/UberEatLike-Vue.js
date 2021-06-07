dd74800dc06f5c9ec48a5e15b20912172ff82e47
Axel_Fradet <fradetaxel@outlook.com>
2021-06-07 13:35:43 +0200

    Add Server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://fradetaxel.fr:2717/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('CONNECTION')
+});
+
 
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
   res.send('Le Serveur est connecté mon bro');
 });
+
+app.get('/api/fonction/1', (req, res) => {
+  res.status(200).json(res)
+  res.json({
+    response : "true"
+  })
+})
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
