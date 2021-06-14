32f3ebf6bd5b545a41bcd5108612960dbbb73c3d
Fradet <fradetaxel@outlook.com>
2021-06-14 15:50:09 +0200

    Fix allow

--- backend/src/serverAuth.ts
+++ backend/src/serverAuth.ts
@@ -21,7 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use('/auth', authRouter);
 
