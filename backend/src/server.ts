83c8581d9eadd1313b69cb7a7c15aefb5c2b321d
Fradet <fradetaxel@outlook.com>
2021-06-14 15:39:35 +0200

    test Acces all

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -46,6 +46,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "fradetaxel.fr"),
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+})
 
 app.use('/login' ,loginRouter);
 
